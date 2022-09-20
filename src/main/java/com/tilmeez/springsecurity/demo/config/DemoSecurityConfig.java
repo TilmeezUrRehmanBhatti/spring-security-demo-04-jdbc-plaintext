23300e50f9bb19c90c0269c47904949d2a6485a5
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-20 12:47:50 +0200

    Add JDBC Authentication

--- src/main/java/com/tilmeez/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/tilmeez/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.tilmeez.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,72 +10,53 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
 
+    // add a reference to our security data source
+    @Autowired
+    private DataSource securityDataSource;
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager() {
+        return new JdbcUserDetailsManager(securityDataSource);
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeRequests(configurer ->
-                        configurer
+                .authorizeRequests(configure ->
+                        configure
                                 .antMatchers("/").hasRole("EMPLOYEE")
                                 .antMatchers("/leaders/**").hasRole("MANAGER")
                                 .antMatchers("/systems/**").hasRole("ADMIN"))
-
-                .formLogin(configurer ->
-                        configurer
+                .formLogin(configure ->
+                        configure
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll())
-
                 .logout(configure ->
                         configure
                                 .permitAll())
-                .exceptionHandling(configurer ->
-                        configurer
+                .exceptionHandling(configure ->
+                        configure
                                 .accessDeniedPage("/access-denied"))
                 .build();
     }
 
+
 /*    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        // add our users for in memory authentication
-
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
-                .withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+        // use jdbc authentication
+        auth.jdbcAuthentication().datasource(securityDataSource)
     }
 
     @Override
